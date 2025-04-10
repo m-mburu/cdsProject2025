de50f7cde7e0871e5cc1f282f18fb42ecc15d47b
vsc37482 <vsc37482@tier2-p-login-2.genius.hpc.kuleuven.be>
2025-04-10 07:05:25 +0200

    first commit
    This is a test code after connecting vscode to hpc

---
+++ test_py.py
@@ -0,0 +1,38 @@
+import collections
+import itertools
+# import matplotlib.pyplot as plt
+# import numpy as np
+import random
+import sys
+import timeit
+
+
+def is_sorted(data):
+    '''check whether a list is sorted
+    
+    Parameters
+    ----------
+    data : list
+        list to check
+        
+    Returns
+    -------
+    bool
+        True if the list is sorted, False otherwise
+    '''
+    for i in range(1, len(data)):
+        # print(data[i])
+        # print(data[i-1])
+        print("----")
+        if data[i- 1] > data[i]:
+            return False
+    return True
+
+## test case best case scenario
+# data = list(range(4))
+# print(data)
+# print(is_sorted(data))
+# test worst case scenario
+data = list(range(4))[::-1]
+print(data)
+print(is_sorted(data))
